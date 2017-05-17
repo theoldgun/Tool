db232b169fe2909f890f7793e2fefb56c8d00102
姬午阳 <@DESKTOP-ECPD362>
2017-05-18 00:17:09 +0900

    d

--- src/cutFile/cutFile.java
+++ src/cutFile/cutFile.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 
 public class cutFile {
+	int w =2;
 	private static Scanner scan;
 	public static void readAndWrite(String readPath, List<String> outPutNameList) throws Exception {
 		File inPutFile = new File(readPath);
